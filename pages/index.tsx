a489ffba12fc348b9d119d90925473b0ac7586d6
HrishGore11 <hrishgore11@gmail.com>
2023-02-13 19:53:42 +0530

    dynamic page renders if slug is availble.

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,19 +36,21 @@ const Home = ({ posts }: Props) => {
           alt=""
         />
       </div>
-      <div className="">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
         {posts.map((post) => (
-          <Link key={post._id} href={`/post/${post.slug.current}`}>
-            <div className="">
-              <h1>I am Post</h1>
-              <img src={urlFor(post.mainImage).url()!} alt="no image" />
+          <Link key={post._id} href={`/posts/${post.slug.current}`}>
+            <div className="border rounded-lg group cursor-pointer overflow-hidden">
+              <img
+                className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+                src={urlFor(post.mainImage).url()!}
+                alt="no image"
+              />
               <div className="flex justify-between p-5 bg-white">
                 <div>
                   <p>{post.title}</p>
                   <p>
                     {post.description} by {post.author.name}
                   </p>
-                  +
                 </div>
                 <img
                   className="h-12 w-12 rounded-full"
