123c6d03e68d9c6e48d80bdb86d8ecd6ef81cd8e
HrishGore11 <hrishgore11@gmail.com>
2023-02-17 19:36:50 +0530

    Finish the Build

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ const Home = ({ posts }: Props) => {
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
+      {/* <Header /> */}
       <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
         <div className="px-10 space-y-5">
           <h1 className="text-6xl max-w-xl font-serif">
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -2,19 +2,54 @@ import { GetStaticProps } from "next";
 import Header from "../../components/Header";
 import { sanityClient, urlFor } from "../../sanity";
 import { Post } from "../../typings";
+import Image from "next/image";
 import PortableText from "react-portable-text";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useState } from "react";
 interface Props {
   post: Post;
 }
 
-function post({ post }: Props) {
-  console.log(post);
+interface FormInput {
+  _id: string;
+  name: string;
+  email: string;
+  comment: string;
+}
 
+function post({ post }: Props) {
+  const [submitted, setSubmitted] = useState(false);
+  console.log(post);
+  console.log(urlFor(post.mainImage).url());
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit: SubmitHandler<FormInput> = (data) => {
+    fetch("/api/createComment", {
+      method: "POST",
+      body: JSON.stringify(data),
+    })
+      .then(() => {
+        console.log(data);
+        setSubmitted(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <main>
-      <Header />
+      {/* <Header /> */}
+
+      <div
+        style={{ width: "100%", height: "100%", position: "relative" }}
+      ></div>
       <img
-        className="h-40 w-full object-cover"
+        className="h-40 w-full object-fill"
         src={urlFor(post.mainImage).url()!}
         alt=""
       />
@@ -63,6 +98,95 @@ function post({ post }: Props) {
           />
         </div>
       </article>
+
+      <hr className=" max-w-lg my-5 mx-auto border-yellow-500" />
+      {submitted ? (
+        <div className="flex flex-col p-10 my-10 bg-yellow-500 text-white max-w-2xl mx-auto">
+          <h3 className="text-3xl font-bold">
+            {" "}
+            Thank you for submitting your comment!
+          </h3>
+          <p>Once it has approved,it will appear below!</p>
+        </div>
+      ) : (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col p-5 max-w-2xl mx-auto
+      mb-10"
+        >
+          <h3 className="text-sm text-yellow-500"> Enjoyed this article?</h3>
+          <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+          <hr className="py-3 mt-2" />
+          <input
+            {...register("_id")}
+            type="hidden"
+            name="_id"
+            value={post._id}
+          />
+          <label className="black-mb-5">
+            <span className="text-gray-700">Name</span>
+            <input
+              {...register("name", { required: true })}
+              type="text"
+              className="shadow border rounded py-2 px-3 form-input mt-1 block w-full
+         ring-yellow-500 outline-none focus:ring"
+              placeholder="John Appleseed"
+            />
+          </label>
+          <label className="black-mb-5">
+            <span className="text-gray-700">Email</span>
+            <input
+              {...register("email", { required: true })}
+              type="email"
+              placeholder="John Appleseed"
+              className="shadow border rounded py-2 px-3 form-input mt-1 block w-full
+         ring-yellow-500 outline-none focus:ring"
+            />
+          </label>
+          <label className="black-mb-5">
+            <span className="text-gray-700">Comment</span>
+            <textarea
+              {...register("comment", { required: true })}
+              rows={8}
+              placeholder="John Appleseed"
+              className="shadow border rounded py-2 px-3 form-textarea mt-1 block ring-yellow-500 w-full outline-none focus:ring
+         "
+            />
+          </label>
+          <div className="flex flex-col p-5">
+            {errors.name && (
+              <span className="text-red-500">- The Name Field is required</span>
+            )}
+            {errors.email && (
+              <span className="text-red-500">
+                - The Email Field is required
+              </span>
+            )}
+            {errors.comment && (
+              <span className="text-red-500">
+                - The Comment Field is required
+              </span>
+            )}
+          </div>
+          <input
+            type="submit"
+            className="shadow bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white
+       font-bold py-2 px-4 rounded cursor-pointer "
+          />
+        </form>
+      )}
+      <div className="flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-y-2">
+        <h3 className="text-4xl">Comments</h3>
+        <hr className="pb-2" />
+        {post.comments.map((comment) => (
+          <div key={comment._id}>
+            <p>
+              <span className="text-yellow-500">{comment.name}: </span>
+              {comment.comment}
+            </p>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
